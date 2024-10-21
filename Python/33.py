3df1329eb4155a50639317d2c2ecbb635bc2dd4b
julianoaureliano <juliano.aureliano2013@gmail.com>
2024-10-21 19:03:38 -0300

    teste

--- Python/33.py
+++ Python/33.py
@@ -1,10 +1,9 @@
 print('a seguir digite 3 numeros:')
 numero1 = int(input('Digite o primeiro:'))
 numero2 = int(input('Digite o segundo:'))
-numero3 = int(input('Digite o terceiro:'))
+numero3 = int(input('Digite o terro:'))
 
 if numero1 :
     numero1 > (numero2,numero3)
     
-
 
